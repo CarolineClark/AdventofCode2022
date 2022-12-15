5b1cb264f459cacf9e3743afb2c9a968f7773e7c
Caroline Clark <5319573+CarolineClark@users.noreply.github.com>
2022-12-15 23:05:04 +0100

    day 1 advent of code

---
+++ ADVENT_OF_CODE/day_1/DotnetStarter.Logic.Tests/AdventOfCode1Test.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace DotnetStarter.Logic.Tests
+{
+    public class HelloWorldTest
+    {
+        [Fact]
+        public void AcceptanceTest()
+        {
+            string contents = File.ReadAllText("../../../caroline.txt");
+            Assert.Equal(0, HelloWorld.CaloriesInBiggestFoodBag(contents));
+        }
+
+        [Fact]
+        public void ExampleInputFinalTest()
+        {
+            var exampleInput = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+            int mostCalories = HelloWorld.CaloriesInBiggestFoodBag(exampleInput);
+            Assert.Equal(24000, mostCalories);
+        }
+
+        [Fact]
+        public void ParsingStringIntoFoodBags()
+        {
+            var exampleInput = @"1000
+2000
+3000
+
+4000";
+            List<FoodBag> foodBags = HelloWorld.ToFoodBags(exampleInput);
+
+            var foodBag1 = new FoodBag(new List<int>() {1000,2000,3000});
+            var foodBag2 = new FoodBag(new List<int>() {4000});
+
+            Assert.Equal(foodBag1.calories, foodBags[0].calories);
+            Assert.Equal(foodBag2.calories, foodBags[1].calories);
+        }
+    }
+}
---
+++ ADVENT_OF_CODE/day_1/DotnetStarter.Logic/AdventOfCode1.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace DotnetStarter.Logic
+{
+    public class HelloWorld
+    {
+        public static string Hello() => "World!";
+
+        public static int CaloriesInBiggestFoodBag(string exampleInput)
+        {
+            var foodbags = ToFoodBags(exampleInput);
+            return foodbags.Select(delegate(FoodBag f)
+            {
+                int sum = 0;
+                foreach (var foodItem in f.calories)
+                {
+                    sum += foodItem;
+                }
+
+                return sum;
+            }).Max();
+        }
+
+        public static List<FoodBag> ToFoodBags(string exampleInput)
+        {
+            var foodBags = new List<FoodBag>();
+            var strings = exampleInput.Split("\n\n");
+            for (int i = 0; i < strings.Length; i++)
+            {
+                var foodBag = strings[i].Split("\n");
+                foodBags.Add(
+                    new FoodBag(foodBag.ToList().Select(IntegerType.FromString).ToList())
+                    );
+            }
+
+            return foodBags;
+        }
+    }
+    
+    public struct FoodBag
+    {
+        public List<int> calories;
+        public FoodBag(List<int> calories)
+        {
+            this.calories = calories;
+        }
+    }
+}
